7dc95ffe94454acfb317b9ec20601877b3e2e6ce
Tchilo <tchiloross@gmail.com>
2022-07-14 16:08:04 +0200

    Style for desktop

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/tablet/image-host.jpg":[["image-host.a035cb48.jpg","assets/tablet/image-host.jpg"],"assets/tablet/image-host.jpg"],"./../assets/desktop/bg-pattern-dots.svg":[["bg-pattern-dots.3d5293f0.svg","assets/desktop/bg-pattern-dots.svg"],"assets/desktop/bg-pattern-dots.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/tablet/image-host.jpg":[["image-host.a035cb48.jpg","assets/tablet/image-host.jpg"],"assets/tablet/image-host.jpg"],"./../assets/desktop/image-host.jpg":[["image-host.43d2a2de.jpg","assets/desktop/image-host.jpg"],"assets/desktop/image-host.jpg"],"./../assets/desktop/bg-pattern-dots.svg":[["bg-pattern-dots.3d5293f0.svg","assets/desktop/bg-pattern-dots.svg"],"assets/desktop/bg-pattern-dots.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
